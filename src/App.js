3f13681eeb0e11290ec134d43dbdbcf681136172
Gabriel Siqueira <gabrielsdo1222@gmail.com>
2020-12-11 13:59:21 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import api from './services/api';
 
 import "./styles.css";
+import { useEffect } from "react";
 
 function App() {
+
+  const {repositories, setRepositories} = useState([]);
+
+  useEffect(()=>{
+      api.get('repositories').then(reponse =>{
+        setRepositories(reponse.data);
+      });
+  },[]);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+      title:'Angular',
+      url:'https://github.com/gsdo12/crudCloud',
+      techs:['AngularCLI']
+    })
+
+    setRepositories([... repositories, reponse.data])
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +30,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key="repository.id">
+          {repository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
